e3bdaf74d0a59128a1b041cef7077211838ee3c9
Ryan Williams <rwilliams@stream9.net>
2015-02-27 14:02:53 -0500

    Updated environment logic defaults to accommodate cli scripts

--- config/environment/environment.php
+++ config/environment/environment.php
@@ -1,13 +1,10 @@
 <?php
 
 
-	if (php_sapi_name() === 'cli') {
-		return 'cli';
-
-	} else if (strstr($_SERVER['HTTP_HOST'], '.local')) {
+	if (strstr($_SERVER['HTTP_HOST'], '.local') || strstr(php_uname('n'), '.local')) {
 		return 'development';
 
-	} else if (strstr($_SERVER['HTTP_HOST'], '.dev')) {
+	} else if (strstr($_SERVER['HTTP_HOST'], '.dev') || strstr(php_uname('n'), 'dev')) {
 		return 'staging';
 
 	} else {
--- config/errors/cli.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	error_reporting(E_ALL & ~ E_NOTICE);
-
-	ini_set('display_errors', true);
-	ini_set('log_errors', true);
