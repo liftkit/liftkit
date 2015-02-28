4c5abf602c9762eac496ebf903c4a7482156eeee
Ryan Williams <rwilliams@stream9.net>
2015-02-28 09:41:52 -0500

    Updated environment logic to include host name only

--- config/environment/environment.php
+++ config/environment/environment.php
@@ -19,10 +19,10 @@
 	 */
 
 
-	if (strstr($_SERVER['HTTP_HOST'], '.local') || strstr(php_uname('n'), '.local')) {
+	if (strstr(php_uname('n'), '.local')) {
 		return 'development';
 
-	} else if (strstr($_SERVER['HTTP_HOST'], '.dev') || strstr(php_uname('n'), 'dev')) {
+	} else if (strstr(php_uname('n'), 'dev')) {
 		return 'staging';
 
 	} else {
